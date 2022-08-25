a8faedb79ef6b9ef43dab61624b90ddf45fba4a9
Sergei <Sergeisafr@mail.ru>
2022-08-25 13:34:59 +0500

    Решение задачи 15 является ли день выходным

---
+++ Задача_15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите цифру от 1 до 7 обзначающую день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if ( day > 5 )
+{
+    Console.Write ("Да, выходной");
+}
+else if ( day <= 5 )
+{
+    Console.Write ("Нет, не выходной");
+}
+
