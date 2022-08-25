dbabd8d9c68856dccfcb6a8cf5edc1b88124b5e0
Sergei <Sergeisafr@mail.ru>
2022-08-25 13:31:40 +0500

    Решение задачи 13 выводит третью цифру заданного числа

---
+++ Задача_13/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int min = 10;
+int fax = 0;
+if (num < 100)
+{
+    fax = 0;
+    
+} 
+else if (num > 100)
+
+ {while (num > 999)
+
+    num = num / 10;
+    fax = (num % min);}
+
+if (fax < 1)
+{
+     Console.Write("Третьей цифры нет");
+}
+else if (fax >= 1)
+{
+   Console.Write(fax); 
+}
